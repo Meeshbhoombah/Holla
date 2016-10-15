49819678e09a4b2573db1defc2241de13e75adcc
Rohan Mishra <rohanmishra2017@gmail.com>
2016-10-15 09:11:08 -0400

    Started Project

---
+++ Holla/Holla/Helper.swift
@@ -0,0 +1,39 @@
+//
+//  API.swift
+//  Holla
+//
+//  Created by Rohan Mishra on 10/14/16.
+//  Copyright © 2016 holla. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Helper {
+    
+    func parseURL(url: String) -> Array<String> {
+        return url.characters.split(separator: "@").map(String.init)
+    }
+    
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.characters.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
